3fbc56f5df83d22900b64d374b7bb3091321981b
Philip Guedes <philipguedes@gmail.com>
2017-08-25 16:00:01 -0300

    Split view.py into components

---
+++ app/run.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from views.main_window import Application
+
+
+def main():
+    # Creating empty window with a title
+    root = Tk()
+    root.title("Bizu do Gafa")
+
+    # Getting the screen infos
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+
+    geometry = '{}x{}+{}+{}'.format(width//2, height//2, width//4, height//4)
+    root.geometry(geometry)
+
+    # Initializing the app
+    Application(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()

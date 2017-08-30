3ecf8417aa97d51b0dfe73234e0cfafa703f0105
Philip Guedes <philipguedes@gmail.com>
2017-08-30 08:23:16 -0300

    Fix presented pdf information

--- app/views/main_window.py
+++ app/views/main_window.py
@@ -1,3 +1,5 @@
+import os
+import arrow
 from tkinter import filedialog, Frame
 from tkinter import *
 from tkinter.ttk import Treeview, Scrollbar
@@ -11,16 +13,21 @@ class Application(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.grid(padx=(10, 0), pady=30)
-        self.pdf_files = set()
         self.csv_file = None
         self.create_widgets()
 
     @property
     def pdf_dict(self):
-        ordered = {}
-        for index, item in enumerate(self.pdf_files):
-            ordered[item] = index
-        return ordered
+        pdfs = {}
+        children = self.file_window.get_children()
+        for child in children:
+            data = self.file_window.item(child)
+            print(data)
+            key = data.get('text')
+            tags = data.get('tags')
+            path = tags.pop(0)
+            pdfs.update({key: path})
+        return pdfs
 
     def create_widgets(self):
         """
@@ -42,7 +49,7 @@ class Application(Frame):
         self.file_window.heading('#0', text='index', anchor=CENTER)
         self.file_window.column('#0', minwidth=50, width=50, stretch=NO)
         self.file_window.heading('#1', text='path', anchor=CENTER)
-        self.file_window.column('#1', minwidth=20, width=300)
+        self.file_window.column('#1', minwidth=20, width=300, anchor=CENTER)
         self.file_window.grid(row=1, rowspan=4, columnspan=4, sticky='nsew')
 
     def __create_pdf_widgets(self):
@@ -85,11 +92,11 @@ class Application(Frame):
 
     def remove_selected_pdf(self):
         selected = self.file_window.focus()
-        item = self.file_window.item(selected)
+        item = self.file_window.item(selected).get('values')
+        name = item.pop(0)
         self.file_window.delete(selected)
-        print(self.pdf_files)
-        print(selected)
         self.update_file_index()
+        self.log_info('{} removed'.format(name))
 
     def load_csv_file(self):
         filetypes = ("csv files", "*.csv"), ("all files", "*.*")
@@ -107,25 +114,36 @@ class Application(Frame):
 
     def update_file_index(self):
         for index, item in enumerate(self.file_window.get_children()):
-            self.file_window.item(item, text=index)
+            self.file_window.item(item, text=index+1)
         self.log_info('updated file index')
 
     def open_pdf_file(self):
         filetypes = ("pdf files", "*.pdf"), ("all files", "*.*")
-        filename = filedialog.askopenfilename(initialdir='~',
+        # TODO: Change this initialdir
+        filename = filedialog.askopenfilename(initialdir='~/dev/pdf-merge',
                                               title="Select file",
                                               filetypes=filetypes)
-        # Check if filename is None                                            
-        self.pdf_files.add(filename)
-        self.file_window.insert('', 'end', text='erro', values=filename)
-        self.update_file_index()
+        # Check if filename is None
+        if os.path.isfile(filename):
+            name = os.path.basename(filename)
+            self.file_window.insert('', 'end', text='erro', values=name, tag=filename)
+            self.update_file_index()
+            
         print(filename)
+        print(self.pdf_dict)
 
     def merge_pdf(self):
         filetypes = ("pdf files", "*.pdf"), ("all files", "*.*")
         filepath = filedialog.asksaveasfilename(initialdir='~',
                                                 title='Save File',
                                                 filetypes=filetypes)
+        
         # Call handler save here
+        
         print(filepath)
         print("file saved")
+
+
+    # Create a logger
+    # With time format
+    # http://arrow.readthedocs.io/en/latest/
